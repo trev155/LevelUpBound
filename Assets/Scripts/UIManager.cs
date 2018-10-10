00dc9dcff6a147efdf06145d18b486ac90ca764f
Trevor <trevku155@gmail.com>
2018-10-09 21:49:23 -0400

    Add a Back Button. Fix canvas to scale better.
    Right now, game mode is set to 18:9 for LG G6.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+    public void BackButtonPressed() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+}
