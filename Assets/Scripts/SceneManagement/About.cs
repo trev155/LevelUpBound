ecc15751ec4d9b8421fbc7cc0718001a8bb2b182
Trevor <trevku155@gmail.com>
2018-11-12 22:13:10 -0500

    About page.
    Completion of the About page.
    Also fixed the victory modal size.

--- Assets/Scripts/SceneManagement/About.cs
+++ Assets/Scripts/SceneManagement/About.cs
@@ -2,13 +2,24 @@
  * Everything to do with the "About" page.
  */
 using UnityEngine;
+using UnityEngine.UI;
 
 public class About : MonoBehaviour {
+    public Image image;
+    public Text imageCaption;
+    public Text aboutText;
+
     private void Awake() {
         AspectRatioManager.AdjustScreen();
+        Theme.SetTheme();
+
+        image.sprite = Resources.Load<Sprite>("AboutImages/ab1");
+        imageCaption.text = "Inspiration for this game can from the popular series of custom maps from SCBW.";
+        aboutText.text = "This game was made mostly for fun. For more information, see: https://github.com/trev155/LevelUpBound";
     }
 
     public void BackButton() {
+        AudioManager.Instance.PlayUISound(AudioManager.BUTTON_DING);
         GameContext.LoadPreviousPage();
     }
 }
