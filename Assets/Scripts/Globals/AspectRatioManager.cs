63c286a85bfafa5d2e4bffacf30e1b867de6bd63
Trevor <trevku155@gmail.com>
2018-12-24 15:13:43 -0500

    Add aspect ratio support for ratios from 0.55 to 0.65

--- Assets/Scripts/Globals/AspectRatioManager.cs
+++ Assets/Scripts/Globals/AspectRatioManager.cs
@@ -15,7 +15,7 @@ public static class AspectRatioManager {
         Debug.Log("Aspect Ratio: " + aspectRatio);
         Scene scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
 
-        // 4:3 portrait
+        // 4:3 portrait (0.70 to 0.80)
         if (EqualsApprox(aspectRatio, 0.75f, 0.05f)) {
             if (scene.name == SceneManager.GetSceneNameString(SceneName.MAIN_MENU)) {
                 AdjustMainMenuSceneElements(0.75f);
@@ -35,6 +35,29 @@ public static class AspectRatioManager {
                 AdjustIntroSceneElements(0.75f);
             } else if (scene.name == SceneManager.GetSceneNameString(SceneName.LEVEL_EDITOR_CREATOR)) {
                 AdjustLevelCreatorSceneElements(0.75f);
+            }
+        }
+
+        // 720x1080 (0.55 to 0.65)
+        if (EqualsApprox(aspectRatio, 0.60f, 0.05f)) {
+            if (scene.name == SceneManager.GetSceneNameString(SceneName.MAIN_MENU)) {
+                AdjustMainMenuSceneElements(0.60f);
+            } else if (scene.name == SceneManager.GetSceneNameString(SceneName.MAIN_GAME)) {
+                AdjustMainGameSceneElements(0.60f);
+            } else if (scene.name == SceneManager.GetSceneNameString(SceneName.LEVEL_SELECTOR)) {
+                AdjustLevelSelectorSceneElements(0.60f);
+            } else if (scene.name == SceneManager.GetSceneNameString(SceneName.OPTIONS)) {
+                AdjustOptionsSceneElements(0.60f);
+            } else if (scene.name == SceneManager.GetSceneNameString(SceneName.ABOUT)) {
+                AdjustAboutSceneElements(0.60f);
+            } else if (scene.name == SceneManager.GetSceneNameString(SceneName.INSTRUCTIONS)) {
+                AdjustInstructionsSceneElements(0.60f);
+            } else if (scene.name == SceneManager.GetSceneNameString(SceneName.LEVEL_EDITOR_MENU)) {
+                AdjustLevelEditorSceneElements(0.60f);
+            } else if (scene.name == SceneManager.GetSceneNameString(SceneName.INTRO)) {
+                AdjustIntroSceneElements(0.60f);
+            } else if (scene.name == SceneManager.GetSceneNameString(SceneName.LEVEL_EDITOR_CREATOR)) {
+                AdjustLevelCreatorSceneElements(0.60f);
             }
         }
     }
@@ -59,7 +82,7 @@ public static class AspectRatioManager {
     }
  
     private static void AdjustMainMenuSceneElements(float aspectRatio) {
-        if (aspectRatio == 0.75) {
+        if (EqualsApprox(aspectRatio, 0.75f, 0.01f)) {
             ResizeCanvasElement("TitleText", 1000, 200);
             SetAnchoredPosition("TitleText", 0, -150);
             ResizeCanvasElement("ScrollLeftButton", 125, 125);
@@ -79,10 +102,31 @@ public static class AspectRatioManager {
             SetAnchoredPosition("Instructions", -250, -1300);
             SetAnchoredPosition("About", 250, -1300);
         }
+
+        if (EqualsApprox(aspectRatio, 0.60f, 0.01f)) {
+            ResizeCanvasElement("TitleText", 1000, 200);
+            SetAnchoredPosition("TitleText", 0, -150);
+            ResizeCanvasElement("ScrollLeftButton", 125, 125);
+            SetAnchoredPosition("ScrollLeftButton", -400, -400);
+            ResizeCanvasElement("ScrollRightButton", 125, 125);
+            SetAnchoredPosition("ScrollRightButton", 400, -400);
+            SetAnchoredPosition("SelectedModeText", 0, -400);
+            ResizeCanvasElement("PlayButton", 400, 150);
+            SetAnchoredPosition("PlayButton", 0, -550);
+            SetAnchoredPosition("LoadButton", 400, -550);
+            SetAnchoredPosition("GameModeSelectedDescription", 0, -800);
+            SetFontSize("GameModeSelectedDescription", 40);
+            SetAnchoredPosition("LevelSelector", -250, -1000);
+            SetAnchoredPosition("LevelEditor", 250, -1000);
+            SetAnchoredPosition("Options", 0, -1200);
+            SetAnchoredPosition("TutorialText", 0, -1350);
+            SetAnchoredPosition("Instructions", -250, -1500);
+            SetAnchoredPosition("About", 250, -1500);
+        }
     }
 
     private static void AdjustMainGameSceneElements(float aspectRatio) {
-        if (aspectRatio == 0.75) {
+        if (EqualsApprox(aspectRatio, 0.75f, 0.01f)) {
             Camera.main.orthographicSize = 6.1f;
             ResizeCanvasElement("BackButton", 100, 100);
             SetAnchoredPosition("BackButton", 50, -50);
@@ -101,10 +145,14 @@ public static class AspectRatioManager {
                 SetAnchoredPosition("RightButton", 180, 100);
             }
         }
+
+        if (EqualsApprox(aspectRatio, 0.60f, 0.01f)) {
+            // none
+        }
     }
 
     private static void AdjustLevelSelectorSceneElements(float aspectRatio) {
-        if (aspectRatio == 0.75) {
+        if (EqualsApprox(aspectRatio, 0.75f, 0.01f)) {
             ResizeCanvasElement("TitleText", 600, 100);
             SetAnchoredPosition("TitleText", 0, -60);
             SetFontSize("TitleText", 70);
@@ -112,6 +160,7 @@ public static class AspectRatioManager {
             SetAnchoredPosition("BackButton", 50, -50);
             ResizeCanvasElement("LevelButtonsArea", 950, 1000);
             SetAnchoredPosition("LevelButtonsArea", 0, -625);
+
             ResizeCanvasElement("ScrollLeftButton", 100, 100);
             SetAnchoredPosition("ScrollLeftButton", -300, 225);
             ResizeCanvasElement("ScrollRightButton", 100, 100);
@@ -122,10 +171,25 @@ public static class AspectRatioManager {
             SetAnchoredPosition("GameModeToggle", 0, 90);
             SetFontSize("ToggleGameModeText", 40);
         }
+
+        if (EqualsApprox(aspectRatio, 0.60f, 0.01f)) {
+            ResizeCanvasElement("LevelButtonsArea", 950, 1000);
+            SetAnchoredPosition("LevelButtonsArea", 0, -750);
+
+            ResizeCanvasElement("ScrollLeftButton", 100, 100);
+            SetAnchoredPosition("ScrollLeftButton", -300, 250);
+            ResizeCanvasElement("ScrollRightButton", 100, 100);
+            SetAnchoredPosition("ScrollRightButton", 300, 250);
+            SetAnchoredPosition("GameModeText", 0, 250);
+            SetFontSize("GameModeText", 50);
+            ResizeCanvasElement("GameModeToggle", 650, 100);
+            SetAnchoredPosition("GameModeToggle", 0, 100);
+            SetFontSize("ToggleGameModeText", 50);
+        }
     }
 
     private static void AdjustOptionsSceneElements(float aspectRatio) {
-        if (aspectRatio == 0.75) {
+        if (EqualsApprox(aspectRatio, 0.75f, 0.01f)) {
             ResizeCanvasElement("TitleText", 500, 175);
             SetAnchoredPosition("TitleText", 0, -60);
             SetFontSize("TitleText", 70);
@@ -174,10 +238,60 @@ public static class AspectRatioManager {
             ResizeCanvasElement("ResetOptions", 300, 125);
             SetAnchoredPosition("ResetOptions", 200, -1330);
         }
+
+        if (EqualsApprox(aspectRatio, 0.60f, 0.01f)) {
+            ResizeCanvasElement("TitleText", 500, 175);
+            SetAnchoredPosition("TitleText", 0, -60);
+            SetFontSize("TitleText", 70);
+            ResizeCanvasElement("BackButton", 100, 100);
+            SetAnchoredPosition("BackButton", 50, -50);
+            SetAnchoredPosition("AudioControlLabel", 0, -200);
+            SetFontSize("AudioControlLabel", 45);
+            ResizeCanvasElement("ToggleAudioButton", 300, 125);
+            SetAnchoredPosition("ToggleAudioButton", -150, -300);
+            ResizeCanvasElement("AudioEnabledImage", 100, 100);
+            SetAnchoredPosition("AudioEnabledImage", 150, -300);
+            ResizeCanvasElement("AudioDisabledImage", 100, 100);
+            SetAnchoredPosition("AudioDisabledImage", 150, -300);
+            SetAnchoredPosition("BackgroundMusicVolumeLabel", 0, -400);
+            SetFontSize("BackgroundMusicVolumeLabel", 45);
+            ResizeCanvasElement("BackgroundMusicVolumeSlider", 900, 100);
+            SetAnchoredPosition("BackgroundMusicVolumeSlider", 0, -470);
+            SetAnchoredPosition("SoundEffectsVolumeLabel", 0, -530);
+            SetFontSize("SoundEffectsVolumeLabel", 45);
+            ResizeCanvasElement("SoundEffectsVolumeSlider", 900, 100);
+            SetAnchoredPosition("SoundEffectsVolumeSlider", 0, -600);
+            SetAnchoredPosition("ThemeLabel", 0, -650);
+            SetFontSize("ThemeLabel", 45);
+            ResizeCanvasElement("ThemeNormal", 250, 100);
+            SetAnchoredPosition("ThemeNormal", -180, -720);
+            ResizeCanvasElement("ThemeLight", 250, 100);
+            SetAnchoredPosition("ThemeLight", 180, -720);
+            ResizeCanvasElement("ThemeDark", 250, 100);
+            SetAnchoredPosition("ThemeDark", -180, -820);
+            ResizeCanvasElement("ThemeVibrant", 250, 100);
+            SetAnchoredPosition("ThemeVibrant", 180, -820);
+            SetAnchoredPosition("ControlsLabel", 0, -920);
+            SetFontSize("ControlsLabel", 45);
+            ResizeCanvasElement("TouchArrows", 300, 125);
+            SetAnchoredPosition("TouchArrows", -180, -1020);
+            ResizeCanvasElement("TouchClick", 300, 125);
+            SetAnchoredPosition("TouchClick", 180, -1020);
+            ResizeCanvasElement("ControlSchemeArrowImage", 400, 170);
+            SetAnchoredPosition("ControlSchemeArrowImage", 0, -1180);
+            ResizeCanvasElement("ControlSchemeClickImage", 400, 170);
+            SetAnchoredPosition("ControlSchemeClickImage", 0, -1180);
+            SetAnchoredPosition("DataLabel", 0, -1310);
+            SetFontSize("DataLabel", 45);
+            ResizeCanvasElement("ResetData", 300, 125);
+            SetAnchoredPosition("ResetData", -200, -1400);
+            ResizeCanvasElement("ResetOptions", 300, 125);
+            SetAnchoredPosition("ResetOptions", 200, -1400);
+        }
     }
        
     private static void AdjustAboutSceneElements(float aspectRatio) {
-        if (aspectRatio == 0.75) {
+        if (EqualsApprox(aspectRatio, 0.75f, 0.01f)) {
             SetAnchoredPosition("TitleText", 0, -60);
             SetFontSize("TitleText", 70);
             ResizeCanvasElement("BackButton", 100, 100);
@@ -189,10 +303,19 @@ public static class AspectRatioManager {
             SetAnchoredPosition("AboutText", 0, -1400);
             SetFontSize("AboutText", 35);
         }
+
+        if (EqualsApprox(aspectRatio, 0.60f, 0.01f)) {
+            ResizeCanvasElement("Image", 600, 700);
+            SetAnchoredPosition("Image", 0, -500);
+            SetAnchoredPosition("ImageCaption", 0, -1200);
+            SetFontSize("ImageCaption", 35);
+            SetAnchoredPosition("AboutText", 0, -1500);
+            SetFontSize("AboutText", 35);
+        }
     }
 
     private static void AdjustInstructionsSceneElements(float aspectRatio) {
-        if (aspectRatio == 0.75) {
+        if (EqualsApprox(aspectRatio, 0.75f, 0.01f)) {
             SetAnchoredPosition("TitleText", 0, -60);
             SetFontSize("TitleText", 70);
             ResizeCanvasElement("BackButton", 100, 100);
@@ -209,29 +332,41 @@ public static class AspectRatioManager {
             SetAnchoredPosition("CurrentPageText", 0, 150);
             SetFontSize("CurrentPageText", 40);
         }
+
+        if (EqualsApprox(aspectRatio, 0.60f, 0.01f)) {
+            // none
+        }
     }
 
     private static void AdjustLevelEditorSceneElements(float aspectRatio) {
-        if (aspectRatio == 0.75) {
+        if (EqualsApprox(aspectRatio, 0.75f, 0.01f)) {
             SetAnchoredPosition("TitleText", 0, -60);
             SetFontSize("TitleText", 70);
             ResizeCanvasElement("BackButton", 100, 100);
             SetAnchoredPosition("BackButton", 50, -50);
         }
+
+        if (EqualsApprox(aspectRatio, 0.60f, 0.01f)) {
+            // none
+        }
     }
 
     private static void AdjustIntroSceneElements(float aspectRatio) {
-        if (aspectRatio == 0.75) {
+        if (EqualsApprox(aspectRatio, 0.75f, 0.01f)) {
             ResizeCanvasElement("CloudOutline", 900, 700);
             SetAnchoredPosition("CloudOutline", 0, -380);
             SetFontSize("TitleText", 120);
             ResizeCanvasElement("TitleText", 600, 500);
             ResizeCanvasElement("Arrow", 400, 400);
         }
+
+        if (EqualsApprox(aspectRatio, 0.60f, 0.01f)) {
+            // none
+        }
     }
 
     private static void AdjustLevelCreatorSceneElements(float aspectRatio) {
-        if (aspectRatio == 0.75) {
+        if (EqualsApprox(aspectRatio, 0.75f, 0.01f)) {
             ResizeCanvasElement("GridContainer", 720, 720);
             SetAnchoredPosition("GridContainer", 0, -400);
             SetAnchoredPosition("0,0", 80, 80);
@@ -264,6 +399,10 @@ public static class AspectRatioManager {
             SetAnchoredPosition("ControlPanel", 220, -1050);
 
             SetAnchoredPosition("FooterPanel", 220, -1340);
+        }
+
+        if (EqualsApprox(aspectRatio, 0.60f, 0.01f)) {
+            // none
         }
     }
 }
